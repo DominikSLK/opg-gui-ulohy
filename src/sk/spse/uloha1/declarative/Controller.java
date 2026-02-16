400551535902e769b8fa3e343fbfd1c575b6be8b
DominikSLK <dominikslk9@gmail.com>
2026-02-16 10:24:12 +0100

    uloha1 declarative

--- src/sk/spse/uloha1/declarative/Controller.java
+++ src/sk/spse/uloha1/declarative/Controller.java
@@ -8,14 +8,28 @@ import javafx.scene.control.TextField;
  */
 public class Controller {
 
-    private int counter = 0;
+    private double celsius = 0;
+    private double fahrenheit = 0;
 
     @FXML
-    private TextField counterField;
+    private TextField celsiusField;
 
     @FXML
-    private void incrementCounter() {
-        counter++;
-        counterField.setText(String.valueOf(counter));
+    private TextField fahrenheitField;
+
+    @FXML
+    private void convertToCtoF() {
+        celsius = Double.parseDouble(celsiusField.getText());
+        fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+        fahrenheitField.setText(String.valueOf(fahrenheit));
+    }
+
+    @FXML
+    private void convertToFtoC() {
+        fahrenheit = Double.parseDouble(fahrenheitField.getText());
+        celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+
+        celsiusField.setText(String.valueOf(celsius));
     }
 }
