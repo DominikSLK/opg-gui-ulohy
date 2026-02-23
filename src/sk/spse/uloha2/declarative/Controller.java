365e63746c3ff2851be49b62932b3e266e3d508a
DominikSLK <dominikslk9@gmail.com>
2026-02-23 10:09:54 +0100

    uloha2 declarative

--- src/sk/spse/uloha2/declarative/Controller.java
+++ src/sk/spse/uloha2/declarative/Controller.java
@@ -1,21 +1,40 @@
 package sk.spse.uloha2.declarative;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 /**
  * Controller pre FXML súbor – obsahuje logiku aplikácie
  */
 public class Controller {
 
-    private int counter = 0;
+    @FXML
+    private TextField usernameField;
 
     @FXML
-    private TextField counterField;
+    private TextField passwordField;
 
     @FXML
-    private void incrementCounter() {
-        counter++;
-        counterField.setText(String.valueOf(counter));
+    private Button registerButton;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private ToggleGroup pohlavieGroup;
+
+    @FXML
+    private void register() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Registrácia užívateľa");
+        alert.setHeaderText("Registrácia prebehla úspešne");
+        alert.setContentText("Uživateľ " + usernameField.getText() + " (" + ((RadioButton)pohlavieGroup.getSelectedToggle()).getText() + ") s heslom " + passwordField.getText() + " bol pridaný do systému");
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void close() {
+        Platform.exit();
     }
 }
