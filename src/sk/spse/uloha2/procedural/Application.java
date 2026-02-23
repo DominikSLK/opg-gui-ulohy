30cde69406b740656a3c9a4dc813ccd609864ac8
DominikSLK <dominikslk9@gmail.com>
2026-02-23 11:10:50 +0100

    uloha2 procedural

--- src/sk/spse/uloha2/procedural/Application.java
+++ src/sk/spse/uloha2/procedural/Application.java
@@ -1,10 +1,17 @@
 package sk.spse.uloha2.procedural;
 
+import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -16,25 +23,67 @@ import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
 
-    private int counter = 0;
-
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Počítadlo:");
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10));
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setAlignment(Pos.CENTER);
 
-        TextField textField = new TextField("0");
+        Label usernameLabel = new Label("Užívateľské meno:");
+        Label passwordLabel = new Label("Heslo:");
+        Label genderLabel = new Label("Pohlavie:");
 
-        Button button = new Button("Plus 1");
+        TextField usernameField = new TextField();
+        usernameField.setPromptText("zadaj meno");
 
-        button.setOnAction(e -> {
-            counter++;
-            textField.setText(String.valueOf(counter));
+        TextField passwordField = new TextField();
+        passwordField.setPromptText("zadaj heslo");
+
+        ToggleGroup pohlavieGroup = new ToggleGroup();
+
+        RadioButton maleRadio = new RadioButton("Muž");
+        maleRadio.setSelected(true);
+        maleRadio.setToggleGroup(pohlavieGroup);
+
+        RadioButton femaleRadio = new RadioButton("Žena");
+        femaleRadio.setToggleGroup(pohlavieGroup);
+
+        HBox genderBox = new HBox(10, maleRadio, femaleRadio);
+        genderBox.setAlignment(Pos.CENTER_LEFT);
+
+        grid.add(usernameLabel, 0, 0);
+        grid.add(usernameField, 1, 0);
+
+        grid.add(passwordLabel, 0, 1);
+        grid.add(passwordField, 1, 1);
+
+        grid.add(genderLabel, 0, 2);
+        grid.add(genderBox, 1, 2);
+
+        Separator separator = new Separator();
+        separator.setPadding(new Insets(0, -10, 0, -10));
+        grid.add(separator, 0, 3, 2, 1);
+
+        Button registerButton = new Button("Registrovať");
+        Button closeButton = new Button("Zavrieť");
+
+        HBox buttonsBox = new HBox(10, registerButton, closeButton);
+        buttonsBox.setAlignment(Pos.CENTER_RIGHT);
+        grid.add(buttonsBox, 0, 4, 2, 1);
+
+        registerButton.setOnAction(e -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Registrácia užívateľa");
+            alert.setHeaderText("Registrácia prebehla úspešne");
+            alert.setContentText("Uživateľ " + usernameField.getText() + " (" + ((RadioButton)pohlavieGroup.getSelectedToggle()).getText() + ") s heslom " + passwordField.getText() + " bol pridaný do systému");
+            alert.showAndWait();
         });
 
-        HBox root = new HBox(20, label, textField, button);
-        root.setPadding(new Insets(10));
+        closeButton.setOnAction(e -> Platform.exit());
 
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(grid);
 
         stage.setTitle("Procedural Application 2");
         stage.setScene(scene);
