1e0302f5c2e3421bf8a0bc86747ee52d9a292390
DominikSLK <dominikslk9@gmail.com>
2026-03-16 11:15:51 +0100

    uloha4 declarative

--- src/sk/spse/uloha4/declarative/Controller.java
+++ src/sk/spse/uloha4/declarative/Controller.java
@@ -1,21 +1,39 @@
 package sk.spse.uloha4.declarative;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 /**
  * Controller pre FXML súbor – obsahuje logiku aplikácie
  */
 public class Controller {
 
-    private int counter = 0;
+    @FXML
+    private VBox vbox1;
+    @FXML
+    private VBox vbox2;
+    @FXML
+    private VBox vbox3;
+    @FXML
+    private VBox vbox4;
+
+    private String getRandomColor() {
+        int color = (int) (Math.random() * 0x1000000);
+        return String.format("#%06X", color);
+    }
 
     @FXML
-    private TextField counterField;
+    private void randomize() {
+        vbox1.setStyle("-fx-background-color: " + getRandomColor());
+        vbox2.setStyle("-fx-background-color: " + getRandomColor());
+        vbox3.setStyle("-fx-background-color: " + getRandomColor());
+        vbox4.setStyle("-fx-background-color: " + getRandomColor());
+    }
 
     @FXML
-    private void incrementCounter() {
-        counter++;
-        counterField.setText(String.valueOf(counter));
+    private void close() {
+        Platform.exit();
     }
 }
