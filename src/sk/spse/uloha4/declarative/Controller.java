a44bbd1e37a59e4f5956d24f7b16c7128e3bda39
DominikSLK <dominikslk9@gmail.com>
2026-04-10 10:45:50 +0200

    uloha 15.4

--- src/sk/spse/uloha4/declarative/Controller.java
+++ src/sk/spse/uloha4/declarative/Controller.java
@@ -2,8 +2,10 @@ package sk.spse.uloha4.declarative;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+import java.util.Random;
+import java.util.Locale;
 
 /**
  * Controller pre FXML súbor – obsahuje logiku aplikácie
@@ -19,17 +21,47 @@ public class Controller {
     @FXML
     private VBox vbox4;
 
+    @FXML
+    private ImageView img1;
+    @FXML
+    private ImageView img2;
+    @FXML
+    private ImageView img3;
+    @FXML
+    private ImageView img4;
+    
+    private Random rand = new Random();
+
     private String getRandomColor() {
         int color = (int) (Math.random() * 0x1000000);
         return String.format("#%06X", color);
     }
 
+    private double getRandomRotate() {
+        return rand.nextDouble() * 360.0;
+    }
+
+    private double getRandomScale() {
+        return 0.5 + rand.nextDouble() * 1.5;
+    }
+
+    private String getRandomOpaque() {
+        double x = rand.nextDouble();
+        String s = String.format(Locale.ROOT, "%.2f", x);
+        return s;
+    }
+
     @FXML
     private void randomize() {
-        vbox1.setStyle("-fx-background-color: " + getRandomColor());
-        vbox2.setStyle("-fx-background-color: " + getRandomColor());
-        vbox3.setStyle("-fx-background-color: " + getRandomColor());
-        vbox4.setStyle("-fx-background-color: " + getRandomColor());
+        vbox1.setStyle("-fx-background-color: " + getRandomColor() + "; -fx-opacity: " + getRandomOpaque());
+        vbox2.setStyle("-fx-background-color: " + getRandomColor() + "; -fx-opacity: " + getRandomOpaque());
+        vbox3.setStyle("-fx-background-color: " + getRandomColor() + "; -fx-opacity: " + getRandomOpaque());
+        vbox4.setStyle("-fx-background-color: " + getRandomColor() + "; -fx-opacity: " + getRandomOpaque());
+
+        img1.setStyle("-fx-rotate: " + getRandomRotate());
+        img2.setStyle("-fx-scale-x: " + getRandomScale());
+        img3.setStyle("-fx-scale-y: " + getRandomScale());
+        img4.setStyle("-fx-scale-x: " + getRandomScale() + "; -fx-scale-y: " + getRandomScale() + "; -fx-rotate: " + getRandomRotate());
     }
 
     @FXML
